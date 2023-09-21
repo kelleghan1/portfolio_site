0acb9f6a0c6edd8a5abb8be370d13b1e2ca388e0
Coleman Kelleghan <imdesigner2012@gmail.com>
2023-09-21 16:54:40 -0700

    Implement codegen

--- src/types/generatedGQLTypes.ts
+++ src/types/generatedGQLTypes.ts
@@ -3,62 +3,64 @@ export type InputMaybe<T> = Maybe<T>
 export type Exact<T extends Record<string, unknown>> = { [K in keyof T]: T[K] }
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
+export type MakeEmpty<T extends Record<string, unknown>, K extends keyof T> = { [_ in K]?: never }
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never }
 /** All built-in and custom scalars, mapped to their actual values */
 export interface Scalars {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
+  ID: { input: string, output: string }
+  String: { input: string, output: string }
+  Boolean: { input: boolean, output: boolean }
+  Int: { input: number, output: number }
+  Float: { input: number, output: number }
 }
 
 export interface Category {
   __typename?: 'Category'
-  id: Scalars['Int']
-  name: Scalars['String']
+  id: Scalars['Int']['output']
+  name: Scalars['String']['output']
 }
 
 export interface Link {
   __typename?: 'Link'
-  id: Scalars['Int']
-  isInternal: Scalars['Boolean']
-  label: Scalars['String']
+  id: Scalars['Int']['output']
+  isInternal: Scalars['Boolean']['output']
+  label: Scalars['String']['output']
   type: LinkType
-  url: Scalars['String']
+  url: Scalars['String']['output']
 }
 
 export interface LinkType {
   __typename?: 'LinkType'
-  id: Scalars['Int']
-  name: Scalars['String']
+  id: Scalars['Int']['output']
+  name: Scalars['String']['output']
 }
 
 export interface PortfolioItem {
   __typename?: 'PortfolioItem'
   categories: Category[]
-  description: Scalars['String']
+  description: Scalars['String']['output']
   githubLinks?: Maybe<Link[]>
   homeImage: ProjectImage
-  id: Scalars['Int']
+  id: Scalars['Int']['output']
   links: Link[]
-  name: Scalars['String']
+  name: Scalars['String']['output']
   primaryImage: ProjectImage
   productLinks?: Maybe<Link[]>
   products: Product[]
-  projectId: Scalars['String']
+  projectId: Scalars['String']['output']
   projectImages: ProjectImage[]
 }
 
 export interface Product {
   __typename?: 'Product'
-  id: Scalars['Int']
-  name: Scalars['String']
+  id: Scalars['Int']['output']
+  name: Scalars['String']['output']
 }
 
 export interface ProjectImage {
   __typename?: 'ProjectImage'
-  id: Scalars['Int']
-  imageUrl: Scalars['String']
+  id: Scalars['Int']['output']
+  imageUrl: Scalars['String']['output']
 }
 
 export interface Query {
