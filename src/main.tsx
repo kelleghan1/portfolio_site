0ee81645d853bbe1d236cbcde7708aadffa9cef9
Coleman Kelleghan <imdesigner2012@gmail.com>
2024-05-01 20:01:49 -0600

    Google analytics 4

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { ApolloClient, InMemoryCache, ApolloProvider, type NormalizedCacheObject } from '@apollo/client'
 import ReactDOM from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import { App } from './App.tsx'
 import './index.css'
 import { PORTFOIO_API_URL } from './utils/constants/environment.ts'
+
+ReactGA.initialize('G-DNVN8G3N5Q')
 
 const apolloCLient = new ApolloClient<NormalizedCacheObject>({
   uri: PORTFOIO_API_URL,
